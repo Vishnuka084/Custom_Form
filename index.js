9ee8dfc8b120306dbdc6ae10eae7ab188f827aaa
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-12-13 19:49:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+* Express (npm i express)
+* mongoose (npm i  mongoose)
+* nodemon ( npm i nodemon  -g(sudo))
+* dotenv ( npm i dotenv) -> sendgrid
+* body-parser (npm i body-parser)
+* bcrypt (npm i bcrypt)
+* jsonwebtoken (npm i jsonwebtoken)
+*
+*
+*  */
