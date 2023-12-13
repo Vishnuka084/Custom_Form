cccff49429c8746a31b4898672f7b25c845858f7
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-12-14 00:37:35 +0530

    userschema updated :

---
+++ controller/UserController.js
@@ -0,0 +1,28 @@
+const userSchema = require('../model/UserSchema');
+const bcrypt = require('bcrypt');
+const salt=10;
+const register = (req, resp) =>{
+    bcrypt.hash(req.body.password,salt,function (err, hash){
+        if (err){
+            resp.status(500).json(err);
+        }
+        const user = new userSchema({
+            fullName:req.body.fullName,
+            password:hash,
+            email:req.body.email,
+            activeState:req.body.activeState
+        });
+        user.save().then(saveResponse=>{
+           resp.status(201).json({'message':'saved!'})
+        }).catch(error=>{
+            resp.status(500).json(error);
+        });
+    })
+}
+const login = (req, resp) =>{
+
+}
+
+module.exports={
+    register,login
+}
