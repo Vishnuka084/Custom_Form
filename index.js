bfa011b2e517b8a8dd5a3d75cf56aaa3c673f676
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-12-21 17:25:33 +0530

    Updated Content

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ const bodyParser = require('body-parser');
 const port = process.env.SERVER_PORT | 3000;
 const app = express();
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:false}));
 
+// parse application/json
 app.use(bodyParser.json);
 
 try{
